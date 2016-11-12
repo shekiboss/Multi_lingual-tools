15d56f8e48b3c7f2248bd2d55a73e69ea9ee6481
NAMAN DEEP SINGH <nmndeep@gmail.com>
2016-11-12 23:47:01 +0530

    Update query_srch.py

--- query_srch.py
+++ query_srch.py
@@ -1,7 +1,7 @@
 import os
 import re
 from collections import Counter
-path='/home/naman/ace-0.9.24/output'
+path='path_to_ace/ace-0.9.24/output'
 srr=r'_human_dmrs.txt'
 ans_fil=[]
 sans_fil=[]
@@ -11,7 +11,7 @@ def srch():
     for filename in sorted(os.listdir(path)):
          matched = re.search(srr,filename)
          if matched:                
-              os.chdir('/home/naman/ace-0.9.24/output')
+              os.chdir('path')
               fil= open(filename,'r')
               ans_fil.append(fil.name)
     tans_fil=ans_fil
@@ -24,7 +24,7 @@ def srch():
                for filename in sorted(os.listdir(path)):
                   matches = re.search(srr,filename)
                   if matches:                
-                      os.chdir('/home/naman/ace-0.9.24/output')
+                      os.chdir('path')
                       fil= open(filename,'r')
                       inF=fil.read()
                       qwe_1 = re.search('(.+?) (?:is|not) present',ab,re.I)
